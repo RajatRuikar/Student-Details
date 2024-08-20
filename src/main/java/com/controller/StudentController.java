39ff1654b8d4df9eecabc06804a4d23f8dbb7fb1
RajatRuikar <rajatruikar12@gmail.com>
2024-08-20 23:17:41 +0530

    named all APIs

--- src/main/java/com/controller/StudentController.java
+++ src/main/java/com/controller/StudentController.java
@@ -22,26 +22,31 @@ public class StudentController {
 	@Autowired
 	StudentService studentService;
 	
+//	http://localhost:8080/
 	@GetMapping("/")
 	public String welcomePage() {
 		return "Welcome Page";
 	}
 	
+//	http://localhost:8080/addstudent
 	@PostMapping("/addstudent")
 	public Student addStudent(@RequestBody Student s) {
 		return studentService.newStudent(s);
 	}
 	
+//	http://localhost:8080/getstudents
 	@GetMapping("/getstudents")
 	public List<Student> getAllStudent() {
 		return studentService.getAllStudent();
 	}
 	
+//	http://localhost:8080/getallsubject
 	@GetMapping("/getallsubject")
 	public List<Subject> getAllSubject() {
 		return studentService.getAllSubject();
 	}
 	
+//	http://localhost:8080/listofallsubjects
 	@GetMapping("/listofallsubjects")
 	public String getAllSubjectName() {
 		return studentService.getListOfAllSubject();
